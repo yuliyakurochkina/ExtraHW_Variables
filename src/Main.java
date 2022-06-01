e03d8945403289e39e84efd59389efb54ef671d7
Yuliya Kurochkina <90004263+yuliyakurochkina@users.noreply.github.com>
2022-06-01 22:26:34 +0300

    Extra exercises 01.06.2022

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //2.2 Переменные Задание 6
+
+        int a = 12;
+        int b = 27;
+        int c = 44;
+        int d = 15;
+        int e = 9;
+
+        int result = a * (b + (c - d * e));
+        if (result < 0) {
+            result = result * -1;
+        }
+
+        System.out.println(result);
+
+        // Задание 7
+
+        int a2 = 5;
+        int b2 = 7;
+
+        a2 = a2 + b2;
+        b2 = a2 - b2;
+        a2 = a2 - b2;
+
+        System.out.println("a2 = " + a2 + ", b2 = " + b2);
+
+        // Задание 8
+
+        int a3 = 878;
+        int b3 = (a3 - ((a3 / 100) * 100)) / 10;
+
+        System.out.println(b3);
+
     }
 }
